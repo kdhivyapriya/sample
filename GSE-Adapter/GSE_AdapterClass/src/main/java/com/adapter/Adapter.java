3657c7a02d27875706ea6816512ee2fae815903e
dhivyapriya_kasiling <dhivyapriya_kasiling@THMTP0071.thbs.india.com>
2019-04-15 11:50:12 +0530

    Pushing to git

---
+++ GSE-Adapter/GSE_AdapterClass/src/main/java/com/adapter/Adapter.java
@@ -0,0 +1,28 @@
+package com.adapter;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.commons.json.JsonUtil;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.apache.synapse.mediators.AbstractMediator;
+import org.json.JSONObject;
+
+public class Adapter extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		 try {
+	            org.apache.axis2.context.MessageContext axis2MessageContext = ((Axis2MessageContext) context)
+	                    .getAxis2MessageContext();
+	            OMElement xmlResponse = context.getEnvelope().getBody().getFirstElement();
+	            xmlResponse.build();
+	            JSONObject jsonObject = new JSONObject(JsonUtil.toJsonString(xmlResponse).toString());
+
+	            //setting the payload as the message payload
+	            JsonUtil.getNewJsonPayload(axis2MessageContext, jsonObject.toString(), true, true);
+	        } catch (Exception e) {
+	            handleException("Error while converting the message", e, context);
+	        }
+	        return true;
+	    }
+	
+}
