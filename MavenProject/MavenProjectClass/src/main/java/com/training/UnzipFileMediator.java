c4b3dc283fcf769621f1bbc18634eeea0253f8ab
dhivyapriya_kasiling <dhivyapriya_kasiling@THMTP0071.thbs.india.com>
2019-04-15 11:55:39 +0530

    Add maven

---
+++ MavenProject/MavenProjectClass/src/main/java/com/training/UnzipFileMediator.java
@@ -0,0 +1,12 @@
+package com.training;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.mediators.AbstractMediator;
+
+public class UnzipFileMediator extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		// TODO Implement your mediation logic here 
+		return true;
+	}
+}
